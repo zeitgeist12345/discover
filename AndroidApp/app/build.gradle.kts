48b24baaf515cfa6aa9452b3a3e08ccfcf539b36
Mohammad Sheraj <mdsheraj123@gmail.com>
2025-08-04 22:05:59 +0530

    Release 4.0

--- AndroidApp/app/build.gradle.kts
+++ AndroidApp/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "3.1"
+        versionName = "4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
