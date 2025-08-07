f1483a7328f1880558bb07de179fee01a0d8450c
Mohammad Sheraj <mdsheraj123@gmail.com>
2025-08-07 08:14:24 +0530

    Release 6.0 actual

--- AndroidApp/app/build.gradle.kts
+++ AndroidApp/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "5.0"
+        versionName = "6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
