055437204dff17fe625fdf4d6a504320b09e406b
Mohammad Sheraj <mdsheraj123@gmail.com>
2025-08-08 22:47:19 +0530

    Fix data schema

--- AndroidApp/app/src/main/java/com/example/discover/data/Link.kt
+++ AndroidApp/app/src/main/java/com/example/discover/data/Link.kt
@@ -6,12 +6,11 @@ data class Link(
     val name: String,
     val url: String,
     val description: String,
-    val category: String = "",
+    val category: String = "user-submitted-mobile",
     val views: Int = 0,
     val likes: Int = 0,
     val dislikes: Int = 0,
     @SerializedName("createdAt")
     val createdAt: String = "",
-    val id: String = "",
-    val active: Boolean = true
-) 
+    val id: String = ""
+)
