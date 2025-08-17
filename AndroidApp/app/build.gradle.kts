6e70af2838e7b2187143d9ae0f87d25c6985503f
Mohammad Sheraj <mdsheraj123@gmail.com>
2025-08-17 16:25:53 +0530

    Release 13.0

--- AndroidApp/app/build.gradle.kts
+++ AndroidApp/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "12.0"
+        versionName = "13.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
