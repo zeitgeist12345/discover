09b9e35ca2644ec17496e87f6b5862dc7138df71
Mohammad Sheraj <mdsheraj123@gmail.com>
2025-08-21 21:48:33 +0530

    Make 14.0 release

--- AndroidApp/app/build.gradle.kts
+++ AndroidApp/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "13.0"
+        versionName = "14.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
