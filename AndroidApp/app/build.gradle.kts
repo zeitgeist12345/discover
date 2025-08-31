d1b2f2e74f03dd7cf5ba300c2a12247b2fee6d26
Mohammad Sheraj <mdsheraj123@gmail.com>
2025-08-31 18:46:50 +0530

    Release 21.0

--- AndroidApp/app/build.gradle.kts
+++ AndroidApp/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "20.0"
+        versionName = "21.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
