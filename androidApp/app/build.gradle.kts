effe7172a1ac89ff21c6b67261fb999390d8a639
Mohammad Sheraj <mdsheraj123@gmail.com>
2025-10-20 22:04:29 +0530

    Release version 28

--- androidApp/app/build.gradle.kts
+++ androidApp/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "27.1"
+        versionName = "28.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
