e672209f27f0d9e6bb455eee251542507f9008dc
Mohammad Sheraj <mdsheraj123@gmail.com>
2025-12-20 02:47:53 +0530

    Release 2.8.9

--- androidApp/app/build.gradle.kts
+++ androidApp/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "2.8.8"
+        versionName = "2.8.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
