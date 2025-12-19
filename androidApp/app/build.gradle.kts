5d02b934167d299fc2a5aca886d177a4a99aab42
Mohammad Sheraj <mdsheraj123@gmail.com>
2025-12-19 22:49:28 +0530

    Release 2.8.3

--- androidApp/app/build.gradle.kts
+++ androidApp/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "2.8.2"
+        versionName = "2.8.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
