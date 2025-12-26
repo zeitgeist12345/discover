0e2ec5b9e8d171e9fcbdb9f5da1896cc1febd52c
Mohammad Sheraj <mdsheraj123@gmail.com>
2025-12-26 18:37:46 +0530

    Release 3.0.0
    - Update static file name.

--- androidApp/app/build.gradle.kts
+++ androidApp/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "2.8.16"
+        versionName = "3.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
