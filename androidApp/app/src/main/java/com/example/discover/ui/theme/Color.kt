7fdcb3b0c845b09cfdb3bd84abcfe31d68d37bd0
Mohammad Sheraj <mdsheraj123@gmail.com>
2026-01-04 15:05:06 +0530

    Add license

--- androidApp/app/src/main/java/com/example/discover/ui/theme/Color.kt
+++ androidApp/app/src/main/java/com/example/discover/ui/theme/Color.kt
@@ -1,3 +1,5 @@
+/* Copyright (c) 2025 Mohammad Sheraj Discover is licensed under India PSL v1. You can use this software according to the terms and conditions of the India PSL v1. You may obtain a copy of India PSL v1 at: https://github.com/abirusabil123/discover/blob/main/IndiaPSL1 THIS SOFTWARE IS PROVIDED ON AN “AS IS” BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE. See the India PSL v1 for more details. */
+
 package com.example.discover.ui.theme
 
 import androidx.compose.ui.graphics.Color
