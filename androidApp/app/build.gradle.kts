18f563e8826a8be4fe61814cd1911f1108d325f3
Mohammad Sheraj <mdsheraj123@gmail.com>
2026-01-12 07:21:35 +0530

    Release 3.1.0

--- androidApp/app/build.gradle.kts
+++ androidApp/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "3.0.2"
+        versionName = "3.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
