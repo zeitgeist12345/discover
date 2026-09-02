86aacf4db2eb2604390f57adbb71ab6481300c10
Mohammad Sheraj <mdsheraj123@gmail.com>
2026-09-02 23:29:28 +0530

    Release 4.2.8

--- androidApp/app/build.gradle.kts
+++ androidApp/app/build.gradle.kts
@@ -20,7 +20,7 @@ configure<ApplicationExtension> {
         minSdk = 26
         targetSdk = 37
         versionCode = 1
-        versionName = "4.2.7"
+        versionName = "4.2.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
